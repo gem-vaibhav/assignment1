c0b9bdf9e5280a0ddafa60cdec28c01827fcd16d
gem-vaibhav <92976392+gem-vaibhav@users.noreply.github.com>
2021-11-11 12:58:55 +0530

    Add files via upload

---
+++ assignment1/question7.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace question7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first_letter, second_letter, third_letter;
+            first_letter = Console.ReadLine()[0];
+            second_letter = Console.ReadLine()[0];
+            third_letter = Console.ReadLine()[0];
+            Console.WriteLine($"{third_letter} {second_letter} {first_letter}");
+        }
+    }
+}
